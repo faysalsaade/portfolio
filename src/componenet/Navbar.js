bdb336267f7a7c02ebbe32124ffe37c174091fe7
faysalsaade <faysalsaadeh@gmail.com>
2024-12-28 16:04:21 +0200

    create new project

--- src/componenet/Navbar.js
+++ src/componenet/Navbar.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="mainNav">
-      <div className="nav">
-        <img
-          src="https://www.howiework.com/static/media/logo.73fe7a7a.png"
-          className="imgNav"
-          alt="logo"
-        />
-        <p className="name">Mahmoud Haddara</p>
+    <>
+      <div className="mainNav">
+        <div className="nav">
+          <img
+            src="https://www.howiework.com/static/media/logo.73fe7a7a.png"
+            className="imgNav"
+            alt="logo"
+          />
+          <p className="name">Mahmoud Haddara</p>
+        </div>
+        {/* second part */}
+        <div className="options">
+          <ul>
+            <li>Works</li>
+            <li>Posts</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <GiHamburgerMenu className="hamb" onClick={() => {}} />
       </div>
-      {/* second part */}
-      <div className="options">
-        <ul>
+      <aside className="aside">
+        <ul className="asideUl">
           <li>Works</li>
           <li>Posts</li>
           <li>About</li>
           <li>Contact</li>
         </ul>
-      </div>
-      <GiHamburgerMenu className="hamb" />
-    </div>
+      </aside>
+    </>
   );
 };
 
