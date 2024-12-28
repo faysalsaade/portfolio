78a7304c94eb5aa990aaba1121ec79b74b71491f
faysalsaade <faysalsaadeh@gmail.com>
2024-12-28 13:05:11 +0200

    create new project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+
+import Hero from "./hero";
 import Navbar from "./componenet/Navbar";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/hero.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="firstpage">
+      <div className="text">
+        <h4 className="h4">Hi, I'm Mahmoud</h4>
+        <p className="pPas">
+          <span className="passionate">Passionate</span>
+          <span className="software">Software Developer</span>
+        </p>
+      </div>
+      <div>
+        <img
+          className="imgPer"
+          src="https://omarwork-com.onrender.com/static/media/sy.5bbea357f77e8437585e.png"
+          alt="Mahmoud Haddara"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
