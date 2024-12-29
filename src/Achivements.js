c25b40f00aab5234f3b82459e693e75ae19619a1
faysalsaade <faysalsaadeh@gmail.com>
2024-12-29 11:17:10 +0200

    create new project

---
+++ src/Achivements.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./achivements.css";
+const Achivements = () => {
+  return <div>achivements</div>;
+};
+
+export default Achivements;
--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./component/Navbar.css";
 
 import Hero from "./hero";
 import Navbar from "./component/Navbar";
+import Achivements from "./achivements";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Achivements />
     </div>
   );
 }
