963a104f43d4b611fa11b3a95e152b1a9feaec1f
faysalsaade <faysalsaadeh@gmail.com>
2024-12-30 11:57:57 +0200

    create new project

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,43 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  function toogle() {
-    open === true ? setOpen(false) : setOpen(true);
-  }
+  const asideRef = useRef(null);
+
+  const toggle = () => setOpen((open) => !open);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (asideRef.current && !asideRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
-      <div className="mainNav">
-        <div className="nav">
-          <img
-            src="https://www.howiework.com/static/media/logo.73fe7a7a.png"
-            className="imgNav"
-            alt="logo"
-          />
-          <p className="name">Mahmoud Haddara</p>
+      <div className="fixedNav">
+        <div className="mainNav">
+          <div className="nav">
+            <img
+              src="https://www.howiework.com/static/media/logo.73fe7a7a.png"
+              className="imgNav"
+              alt="logo"
+            />
+            <p className="name">Mahmoud Haddara</p>
+          </div>
+
+          {/* second part */}
+          <div className="options">
+            <ul>
+              <li>
+                <a href="#works">Works</a>
+              </li>
+              <li>
+                <a href="#posts">Posts</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          <GiHamburgerMenu className="hamb" onClick={toggle} />
         </div>
-        {/* second part */}
-        <div className="options">
-          <ul>
-            <li>Works</li>
-            <li>Posts</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        <GiHamburgerMenu className="hamb" onClick={toogle} />
       </div>
-      {open === true ? (
-        <aside className="aside">
+
+      {open && (
+        <aside ref={asideRef} className="aside">
           <ul className="asideUl">
-            <li>Works</li>
-            <li>Posts</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>
+              <a href="#works">Works</a>
+            </li>
+            <li>
+              <a href="#posts">Posts</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
         </aside>
-      ) : null}
+      )}
     </>
   );
 };
--- src/hero.js
+++ src/hero.js
@@ -3,49 +3,52 @@ import "./hero.css";
 const Hero = () => {
   return (
     <>
-      <div className="firstpage">
-        <div className="text">
-          <h4 className="h4">Hi, I'm Mahmoud</h4>
-          <p className="pPas">
-            <span className="passionate">Passionate</span>
-            <span className="software">Software Developer</span>
-          </p>
+      <div id="about">
+        <div className="firstpage">
+          <div className="text">
+            <h4 className="h4">Hi, I'm Mahmoud</h4>
+            <p className="pPas">
+              <span className="passionate">Passionate</span>
+              <span className="software">Software Developer</span>
+            </p>
+          </div>
+          <div>
+            <img
+              className="imgPer"
+              src="https://omarwork-com.onrender.com/static/media/sy.5bbea357f77e8437585e.png"
+              alt="Mahmoud Haddara"
+            ></img>
+          </div>
         </div>
-        <div>
-          <img
-            className="imgPer"
-            src="https://omarwork-com.onrender.com/static/media/sy.5bbea357f77e8437585e.png"
-            alt="Mahmoud Haddara"
-          ></img>
-        </div>
-      </div>
-      <div className="container">
-        <p className="telepaty">
-          {" "}
-          My name is Mahmoud Haddara. Currently working at{" "}
-          <a className="A.telepaty" href="https://telepaty.com/">
-            Telepaty Holding .
-          </a>{" "}
-        </p>
-        <div className="description">
-          <p>
-            As a software developer, I have a passion for creating innovative
-            solutions using cutting-edge technology.
+        <div className="container">
+          <p className="telepaty">
+            {" "}
+            My name is Mahmoud Haddara. Currently working at{" "}
+            <a className="A.telepaty" href="https://telepaty.com/">
+              Telepaty Holding .
+            </a>{" "}
           </p>
-          <br></br>
-          <p>
-            I am constantly learning and staying up-to-date with the latest
-            developments in the field, and I bring this expertise and drive to
-            every project I work on. Whether working independently or as part of
-            a team, I consistently strive to deliver the best possible outcome.
-          </p>
-          <br></br>
-          <p>
-            In my spare time, I enjoy doing some freelancing work. My goal is to
-            use technology to help clients articulate what’s in their mind to
-            better express themselves.
-          </p>
-          <br></br>
+          <div className="description">
+            <p>
+              As a software developer, I have a passion for creating innovative
+              solutions using cutting-edge technology.
+            </p>
+            <br></br>
+            <p>
+              I am constantly learning and staying up-to-date with the latest
+              developments in the field, and I bring this expertise and drive to
+              every project I work on. Whether working independently or as part
+              of a team, I consistently strive to deliver the best possible
+              outcome.
+            </p>
+            <br></br>
+            <p>
+              In my spare time, I enjoy doing some freelancing work. My goal is
+              to use technology to help clients articulate what’s in their mind
+              to better express themselves.
+            </p>
+            <br></br>
+          </div>
         </div>
       </div>
     </>
