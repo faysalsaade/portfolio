7bf9fe637f266b5dbc9298d83c150bb347af54e0
faysalsaade <faysalsaadeh@gmail.com>
2024-12-30 22:08:47 +0200

    create new project

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    title: "Snap App",
+    img: "https://github.com/omarsaade/omarsaade/raw/main/assets/pp.jpg?raw=true",
+    tags: ["React", "Node", "ExpressJS", "MongoDB"],
+    projectLink: "https://omarsaade.github.io/snap-app/",
+    githubLink: "https://github.com/omarsaade/Snap-App",
+  },
+  {
+    title: "Al Markazia News App",
+    img: "https://user-images.githubusercontent.com/49005530/235341454-724e8957-a55b-4d8c-a51c-d2d088c5f333.jpg",
+    tags: ["ReactNatve", "Redux", "ReduxToolkit"],
+    projectLink: "https://github.com/omarsaade/almarkazia",
+    githubLink: "https://github.com/omarsaade/almarkazia",
+  },
+  {
+    title: "CryptoFinder App",
+    img: "https://omarwork-com.onrender.com/static/media/Untitled1.939f34a0bf394b4cbd80.jpg",
+    tags: ["React", "Firebase", "ReduxToolkit", "Render"],
+    projectLink: "https://github.com/omarsaade/CryptoFinder",
+    githubLink: "https://github.com/omarsaade/CryptoFinder",
+  },
+];
