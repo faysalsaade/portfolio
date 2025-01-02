9a51a0ecaa2c2d1b004ebf90520e823dc63ebb8c
faysalsaade <faysalsaadeh@gmail.com>
2025-01-02 19:36:09 +0200

    create new project

--- src/component/Achievements.js
+++ src/component/Achievements.js
@@ -7,8 +7,8 @@ const Achievements = () => {
         id="works"
         className="-z-10 relative flex items-center justify-center flex-col m-auto w-2/3 lg:top-36 sm:top-40 top-40"
       >
-        <div className="flex flex-col items-start w-8/12">
-          <div className="flex">
+        <div className="flex flex-col items-start w-full lg:w-8/12">
+          <div className="flex relative left-3">
             <p className=" bg-orange-600 h-7 w-2 right-3 relative"></p>
             <p className="sm:text-xl font-bold text-sm ">
               Some things I've built
@@ -18,7 +18,7 @@ const Achievements = () => {
         {data.map((ele) => (
           <>
             <p className="my-6  sm:text-xl font-bold text-sm ">{ele.title}</p>
-            <img className="w-2/3" src={ele.img} alt="github" />
+            <img className="sm:w-2/3 w-full" src={ele.img} alt="github" />
             <div className="flex flex-col text-xl gap-5 w-2/3 relative top-5">
               <ul className="flex items-start justify-around w-64">
                 {ele.tags.map((lang) => (
@@ -27,7 +27,7 @@ const Achievements = () => {
                   </li>
                 ))}
               </ul>
-              <p className="text-[#716D69] text-sm sm:text-2xl">{ele.text}</p>
+              <p className="text-[#716D69] text-sm sm:text-2xl ">{ele.text}</p>
               <div className=" flex items-center justify-between w-48 my-5 ">
                 <a href={ele.projectLink} className="cursor-pointer">
                   View Project
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
     <>
       <div
         id="about"
-        className="flex justify-around items-center 
+        className="flex justify-around items-center  z-10
         top-0 fixed w-full backdrop-blur h-16  "
       >
         <div className="flex items-center">
