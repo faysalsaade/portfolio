afb3f63f2cf44aad21ad0372acb7ef1b7a54cbed
faysalsaade <faysalsaadeh@gmail.com>
2025-01-11 21:46:19 +0200

    create new project

--- src/component/contact.js
+++ src/component/contact.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
 import { SocialMedia } from "../constants";
 const Contact = () => {
+  const [open, setOpen] = useState(false);
+  const asideRef = useRef(null);
+
+  const toggle = () => setOpen((open) => !open);
+
+  useEffect(() => {
+    const ClickOutside = (event) => {
+      if (asideRef.current && !asideRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", ClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", ClickOutside);
+    };
+  }, []);
   return (
     <>
       <div className="relative top-48 left-7 sm:left-16 2xl:max-w-2xl xl:m-auto md:max-w-2xl  md:m-auto w-9/12 m-auto  ">
@@ -22,7 +40,10 @@ const Contact = () => {
               <MdOutlineArrowOutward />
             </a>
           ))}
-          <button className="py-2  w-40 bg-orange-600 text-white rounded-md top-8 relative font-normal mb-[-55px] hover:bg-[#c34e1f]">
+          <button
+            className="py-2  w-40 bg-orange-600 text-white rounded-md top-8 relative font-normal mb-[-55px] hover:bg-[#c34e1f]"
+            onClick={toggle}
+          >
             Send Message
           </button>
         </div>
@@ -30,31 +51,35 @@ const Contact = () => {
           © Designed & Built by Mahmoud Haddara, 2025
         </footer>
       </div>
-      <div className="backdrop-blur-sm bg-[hsla(0,0%,100%,.95)] rounded-2xl shadow-md fixed left-[28%] top-[180px] w-[37vw] z-30 hidden">
-        <form className="flex flex-col h-fit py-2 px-8">
-          <div className="my-5 mx-0">
-            <label className="text-[#252422] font-bold text-s mb-2">Name</label>
-            <input className="border border-[#ccc] rounded-[2px] py-1 px-2 w-[100%]" />
-          </div>
-          <div className="my-5 mx-0">
-            <label className="text-[#252422] font-bold text-s mb-2">
-              Email
-            </label>
-            <input className="border border-[#ccc] rounded-[2px]  py-1 px-2 w-[100%]" />
-          </div>
-          <div className="my-5 mx-0">
-            <label className="text-[#252422] font-bold text-s mb-2">
-              Message
-            </label>
-            <textarea className="bg-[#fff] border border-[#ccc] rounded-[2px] p-[0.2rem] px-[0.4rem] w-full"></textarea>
-          </div>
-          <div className="flex justify-end">
-            <button className="bg-[#d36d45] text-[#bebdbd] cursor-not-allowed px-[17px] p-[12px] text-base  rounded-[5px] border-white border">
-              Send Message{" "}
-            </button>
-          </div>
-        </form>
-      </div>
+      {open && (
+        <div className="backdrop-blur-sm bg-[hsla(0,0%,100%,.95)] rounded-2xl shadow-md fixed left-[28%] top-[180px] w-[37vw] z-30 ">
+          <form className="flex flex-col h-fit py-2 px-8" ref={asideRef}>
+            <div className="my-5 mx-0">
+              <label className="text-[#252422] font-bold text-s mb-2">
+                Name
+              </label>
+              <input className="border border-[#ccc] rounded-[2px] py-1 px-2 w-[100%]" />
+            </div>
+            <div className="my-5 mx-0">
+              <label className="text-[#252422] font-bold text-s mb-2">
+                Email
+              </label>
+              <input className="border border-[#ccc] rounded-[2px]  py-1 px-2 w-[100%]" />
+            </div>
+            <div className="my-5 mx-0">
+              <label className="text-[#252422] font-bold text-s mb-2">
+                Message
+              </label>
+              <textarea className="bg-[#fff] border border-[#ccc] rounded-[2px] p-[0.2rem] px-[0.4rem] w-full"></textarea>
+            </div>
+            <div className="flex justify-end">
+              <button className="bg-[#d36d45] text-[#bebdbd] cursor-not-allowed px-[17px] p-[12px] text-base  rounded-[5px] border-white border">
+                Send Message{" "}
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
